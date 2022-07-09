b6546201dacd8454e5cd97bc3053feac41b48bae
thanhphat nguyen <thanhphatnguyen83@gmail.com>
2022-07-09 21:48:44 +0700

    Login

--- Project_Apple/Program.cs
+++ Project_Apple/Program.cs
@@ -16,7 +16,7 @@ namespace Project_Apple
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmQuanLy());
+            Application.Run(new frmDangnhap());
         }
     }
 }
